7c3ce58f582d49f9b73ee8822da32a03f0072290
Ondrej Vesely <265290@vutbr.cz>
2026-04-16 15:01:05 +0200

    dd

--- searching.py
+++ searching.py
@@ -1,20 +1,27 @@
 import os
-
+import json
 # get current working directory path
 cwd_path = os.getcwd()
 
 
 def read_data(file_name, field):
     """
-    Reads json file and returns sequential data.
-    :param file_name: (str), name of json file
-    :param field: (str), field of a dict to return
-    :return: (list, string),
-    """
+       Reads json file and returns sequential data.
+       :param file_name: (str), name of json file
+       :param field: (str), field of a dict to return
+       :return: (list, string),
+       """
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, "r") as file:
+        data = json.load(file)
+        if field in data:
+            return data[field]
+
 
 
 def main():
+    sequential_data  = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
     pass
 
 
