221c876933ca3db3b91ab56054df0edfcaa9ed6c
Ondrej Vesely <265290@vutbr.cz>
2026-04-16 15:52:09 +0200

    dgl

--- searching.py
+++ searching.py
@@ -30,11 +30,33 @@ def linear_search(sekvence, cislo):
     cislo_kolikrat = count
     plp = set(index)
     return {"positions":index,"count": cislo_kolikrat}
-print(linear_search([54, 2, 18, 5, 3, 31, 20, 65, -10, 300, 17, 5, -1, 0, 0, 102, 7, 8, 9, 9, -3, -5, 0, 1, 63, 82, -36, -5], 5))
+
+def binary_search(seznam, cislo):
+    prostřed = len(seznam) / 2
+    stred1 = int(prostřed)
+    vlv = seznam[stred1]
+    leva = 0
+    prava = len(seznam) - 1
+    while leva <= prava:
+        střed = (leva + prava) // 2
+        střed_hodnota = seznam[střed]
+        if střed_hodnota == cislo:
+            return střed
+        elif střed_hodnota < cislo:
+            leva = střed + 1
+        else:
+            prava = střed - 1
+    return None
+
+
 
 def main():
     sequential_data  = read_data("sequential.json", "unordered_numbers")
+    sequential_data2 = read_data("sequential.json", "ordered_numbers")
     print(sequential_data)
+    print(sequential_data2)
+    print(linear_search( [54, 2, 18, 5, 3, 31, 20, 65, -10, 300, 17, 5, -1, 0, 0, 102, 7, 8, 9, 9, -3, -5, 0, 1, 63, 82, -36, -5], 5))
+    print(binary_search(sequential_data2, 13))
     pass
 
 
